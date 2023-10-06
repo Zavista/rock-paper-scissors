9673f301578833c8c886b4ca8ba1fd6780df54c8
ZhaDav <84294353+Zavista@users.noreply.github.com>
2023-10-06 00:13:15 -0400

    Created getComputerChoice() in script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("hi");
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+function getComputerChoice(){
+    let rand = Math.floor(Math.random() * 3); //Returns a random integer from 0 to 2 (inclusive)
+    if (rand == 0){
+        return "Rock";
+    }
+    else if (rand == 1){
+        return "Paper";
+    }
+    else{
+        return "Scissor";
+    }
+}
